f010882f7935c820cd4109456040ec001686eb1b
David G Chaves <davidgchaves@gmail.com>
2014-05-19 19:14:15 +0200

    Complete item model for accessing and storing items

--- src/procraslistnator/core.clj
+++ src/procraslistnator/core.clj
@@ -1,15 +1,20 @@
 (ns procraslistnator.core
+  (:require [procraslistnator.item.model :as items])
   (:require [ring.adapter.jetty :as ring]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [not-found]]))
+
+(def db "jdbc:postgresql://localhost/procraslistnator")
 
 (defroutes routes
   (GET "/" [] "<h2>Home Page</h2>")
   (not-found "Page not found."))
 
 (defn -main [port]
+  (items/create-table db)
   (ring/run-jetty routes {:port (Integer. port)}))
 
 (defn -dev-main [port]
+  (items/create-table db)
   (ring/run-jetty (wrap-reload #'routes) {:port (Integer. port)}))
