f7b1976cd1f747448d480f55f7513baaefbfe270
David G Chaves <davidgchaves@gmail.com>
2014-05-19 19:23:03 +0200

    Add '/pretty-request' route to inspect ring-request in the browser

--- src/procraslistnator/core.clj
+++ src/procraslistnator/core.clj
@@ -2,6 +2,7 @@
   (:require [procraslistnator.item.model :as items])
   (:require [ring.adapter.jetty :as ring]
             [ring.middleware.reload :refer [wrap-reload]]
+            [ring.handler.dump :refer [handle-dump]]
             [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
             [compojure.route :refer [not-found]]))
 
@@ -9,12 +10,16 @@
 
 (defroutes routes
   (GET "/" [] "<h2>Home Page</h2>")
+  (GET "/pretty-request" [] handle-dump)
   (not-found "Page not found."))
+
+(def app
+  routes)
 
 (defn -main [port]
   (items/create-table db)
-  (ring/run-jetty routes {:port (Integer. port)}))
+  (ring/run-jetty app {:port (Integer. port)}))
 
 (defn -dev-main [port]
   (items/create-table db)
-  (ring/run-jetty (wrap-reload #'routes) {:port (Integer. port)}))
+  (ring/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
