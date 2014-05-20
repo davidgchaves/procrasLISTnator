29a293dd80bb89de3f863ccda94e19ffe53ec26c
David G Chaves <davidgchaves@gmail.com>
2014-05-20 17:29:56 +0200

    Display items in 'items-page'

--- src/procraslistnator/item/view.clj
+++ src/procraslistnator/item/view.clj
@@ -11,6 +11,20 @@
           [:link {:href "/bootstrap-3.1.1/css/bootstrap.min.css"
                   :rel :stylesheet}]]
          [:body
-          [:div.container]
+          [:div.container
+           [:h1 "My Items"]
+           [:div.row
+            (if (seq items)
+              [:table.table.table-striped
+               [:thead
+                [:tr
+                 [:th "Name"]
+                 [:th "Description"]]]
+               [:tbody
+                (for [i items]
+                  [:tr
+                   [:th (h (:name i))]
+                   [:th (h (:description i))]])]]
+              [:div.col-sm-offset-1 "There are no items."])]]
           [:script {:src "http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"}]
           [:script {:src "bootstrap-3.1.1/css/bootstrap.min.css"}]]))
