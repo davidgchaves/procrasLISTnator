943d2d3b6440bd5a67fcb4f8e27d0c1e33833851
David G Chaves <davidgchaves@gmail.com>
2014-05-20 17:48:53 +0200

    Add 'create-item' form

--- src/procraslistnator/item/view.clj
+++ src/procraslistnator/item/view.clj
@@ -2,6 +2,33 @@
   (:require [hiccup.page :refer [html5]]
             [hiccup.core :refer [html h]]))
 
+(defn new-item []
+  (html
+    [:form.form-horizontal
+     {:method "POST" :action "/items"}
+
+     [:div.form-group
+      [:label.control-label.col-sm-2 {:for :name-input}
+       "Name"]
+      [:div.col-sm-10
+       [:input#name-input.form-control
+        {:name :name
+         :placeholder "Name"}]]]
+
+     [:div.form-group
+      [:label.control-label.col-sm-2 {:for :desc-input}
+       "Description"]
+      [:div.col-sm-10
+       [:input#desc-input.form-control
+        {:name :description
+         :placeholder "Description"}]]]
+
+     [:div.form-group
+      [:div.col-sm-offset-2.col-sm-10
+       [:input.btn.btn-primary
+        {:type :submit
+         :value "New item"}]]]]))
+
 (defn items-page [items]
   (html5 {:lang :en}
          [:head
@@ -10,9 +37,11 @@
                   :content "width=device-width, initial-scale=1.0"}]
           [:link {:href "/bootstrap-3.1.1/css/bootstrap.min.css"
                   :rel :stylesheet}]]
+
          [:body
           [:div.container
            [:h1 "My Items"]
+
            [:div.row
             (if (seq items)
               [:table.table.table-striped
@@ -25,6 +54,11 @@
                   [:tr
                    [:th (h (:name i))]
                    [:th (h (:description i))]])]]
-              [:div.col-sm-offset-1 "There are no items."])]]
+              [:div.col-sm-offset-1 "There are no items."])]
+
+           [:div.col-sm-6
+            [:h2 "Create a new item"]
+            (new-item)]]
+
           [:script {:src "http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"}]
           [:script {:src "bootstrap-3.1.1/css/bootstrap.min.css"}]]))
